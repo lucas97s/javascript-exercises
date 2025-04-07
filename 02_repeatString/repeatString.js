c950345f67e867ce991fe3563ede3ba8b998cbf9
lucas97s <67008883+lucas97s@users.noreply.github.com>
2025-04-08 02:12:39 +0800

    Complete repeatString task

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(repeatedString, numberOfRepeates) {
+    let concatedString = "";
+    if (numberOfRepeates < 0 ){
+        return "ERROR" ; 
+    } 
+    for (let i = 1 ; i<= numberOfRepeates ; i++){
+        concatedString += repeatedString ; 
+    }
+    return concatedString
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;

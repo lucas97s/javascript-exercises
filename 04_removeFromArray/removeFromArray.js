ae74fb75ab3fe3fa0685e441e79c09dac5259602
lucas97s <67008883+lucas97s@users.noreply.github.com>
2025-04-08 07:04:12 +0800

    Add removeArray excersice to the branch
    I learned that Apply.Filter is powerful
    way to filter arrays.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array , ...theArg) {
+    let initialArray = array ; 
+    let index = 0 ; 
+
+    for (const arg of theArg) {
+        index = initialArray.indexOf(arg) ; 
+        while ( index >= 0 ){
+            initialArray.splice(index,1);
+            index = initialArray.indexOf(arg) ;
+        }
+
+    }
+    return initialArray; 
 };
 
-// Do not edit below this line
+// Do not edit be low this line
 module.exports = removeFromArray;

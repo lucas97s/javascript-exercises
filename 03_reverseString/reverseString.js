a163e0c8e431643a9fa9002e47a4404599b17476
lucas97s <67008883+lucas97s@users.noreply.github.com>
2025-04-08 05:49:18 +0800

    Add the modified solution to the reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let lengthOfString = string.length ; 
+    let reversedString = "" ;
+    let stringToReverse = string ;  
+    for (let i = lengthOfString - 1 ; i >= 0 ; i--){
+        reversedString = reversedString.concat(stringToReverse[i])
+    }
+    return reversedString ; 
 };
 
 // Do not edit below this line

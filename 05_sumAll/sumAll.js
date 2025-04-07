f0ca19eb31cb82c1e145ffe706879480b167912a
lucas97s <67008883+lucas97s@users.noreply.github.com>
2025-04-08 07:48:10 +0800

    Complete sumAll related tasks

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    let finalSum = 0 ; 
+
+
+    if (!(Number.isInteger(int1) && Number.isInteger(int2))) return "ERROR" ; 
+    if (!(int1>0 && int2>0)) return "ERROR" ; 
+    if (int1 > int2) {
+        temp = int2 ;
+        int2 = int1 ; 
+        int1 = temp ; 
+    }
+        
+
+    for (let i=int1; i <= int2 ; i++){
+        finalSum += i ; 
+    }
+
+    return finalSum ; 
+
 
 };
 

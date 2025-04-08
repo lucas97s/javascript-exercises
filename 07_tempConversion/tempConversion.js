8aa320716b66234dd46366c4d4d12897d1bcfe7f
lucas97s <67008883+lucas97s@users.noreply.github.com>
2025-04-08 09:57:39 +0800

    Add the leap year and tempconversion task

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,24 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(tempInFahrenheit) {
+  let roundedValue = (((tempInFahrenheit-32)*5)/9).toFixed(1) ;
 
-const convertToFahrenheit = function() {
+  if (roundedValue.endsWith(0)) {
+    return parseInt(parseFloat(roundedValue).toFixed(0)) ; 
+  } else {
+    return parseFloat(roundedValue) ; 
+  }
+
+  
+}
+
+const convertToFahrenheit = function(tempInCelsius) {
+  let roundedValue = ((tempInCelsius*(9/5))+32).toFixed(1) ;
+
+  if (roundedValue.endsWith(0)) {
+    return parseInt(parseFloat(roundedValue).toFixed(0)) ; 
+  } else {
+    return parseFloat(roundedValue) ; 
+  }
+ 
 };
 
 // Do not edit below this line
